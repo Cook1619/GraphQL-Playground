5cceac81d0d4bf4553bd4bd5a87c2d69f7376097
Matt Cook <mathew.cook@mastery.net>
2021-10-11 16:10:14 -0500

    Creating first entity to hookup to mongo db

---
+++ graphql-nest-mongo/src/lesson/lesson.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, ObjectIdColumn, PrimaryColumn } from 'typeorm';
+
+// decorated letting typeorm that thisis a table
+@Entity()
+export class Lesson {
+  // mongo specific
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: string;
+
+  @Column()
+  endDate: string;
+}
