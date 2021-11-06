0448674bf483cf8f738e8851514dfe8def9a62a6
Matt Cook <mathew.cook@mastery.net>
2021-11-06 15:41:04 -0500

    Setting up comment mutation

--- graphql-api/graphql-basics/src/index.js
+++ graphql-api/graphql-basics/src/index.js
@@ -63,6 +63,7 @@ const typeDefs = `
       createPost(data: CreatePostInput!): Post!
       deletePost(id: ID!): Post!
       createComment(data: CreateCommentInput!): Comment!
+      deleteComment(id: ID!): Comment!
     }
 
     input CreateUserInput {
@@ -227,6 +228,17 @@ const resolvers = {
       comments.push(comment);
 
       return comment;
+    },
+    deleteComment(parent, args, context, info) {
+      const commentIndex = comments.findIndex(
+        (comment) => comment.id === args.id
+      );
+      if (commentIndex === -1) {
+        throw new Error("Comment not found");
+      }
+      const deletedComment = comments.splice(commentIndex, 1);
+
+      return deletedComment[0];
     },
   },
   Post: {
